69073b9ff13f11cd51dbf1fe1c43bb749801f192
Daemian Mack <daemianmack@gmail.com>
2013-04-25 20:30:50 -0300

    Typo in cors project.clj

--- cors/project.clj
+++ cors/project.clj
@@ -1,4 +1,4 @@
-0;95;c; Copyright 2013 Relevance, Inc.
+; Copyright 2013 Relevance, Inc.
 
 ; The use and distribution terms for this software are covered by the
 ; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0)
